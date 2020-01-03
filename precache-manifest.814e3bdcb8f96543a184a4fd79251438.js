96e4f4d77a99b623643a03cfdb0ec42ec1583fbc
Иван Лавренов <Viduck27@gmail.com>
2020-01-03 12:27:16 +0300

    Updates

---
+++ precache-manifest.814e3bdcb8f96543a184a4fd79251438.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d092faecf1d229fcaf9e4a454f31c8",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "2401c3e0374c27ae7c4d",
+    "url": "/myapp/static/css/main.af696a07.chunk.css"
+  },
+  {
+    "revision": "7a9ee02f5b87ca3289a7",
+    "url": "/myapp/static/js/2.c688961e.chunk.js"
+  },
+  {
+    "revision": "e8901dbb7c0dc9f342ea385f3c323b35",
+    "url": "/myapp/static/js/2.c688961e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2401c3e0374c27ae7c4d",
+    "url": "/myapp/static/js/main.14b98856.chunk.js"
+  },
+  {
+    "revision": "cd953bfc9fdbc1210fd6",
+    "url": "/myapp/static/js/runtime-main.5f507148.js"
+  },
+  {
+    "revision": "cecff531a2b816e6376a399356bb151b",
+    "url": "/myapp/static/media/avatar.cecff531.jpg"
+  },
+  {
+    "revision": "c3b275793612c177f0aadef036079acb",
+    "url": "/myapp/static/media/error.c3b27579.png"
+  },
+  {
+    "revision": "2e43c009ccdb6054c9ff5f4a6a42c0a0",
+    "url": "/myapp/static/media/preloader.2e43c009.gif"
+  }
+]);
